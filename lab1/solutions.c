c5a4be4b18514aedc592454903ba100c24f121c1
Vovan41k3 <vova.patr3@gmail.com>
2025-11-05 04:12:36 +0300

    первые задания

--- lab1/solutions.c
+++ lab1/solutions.c
@@ -4,47 +4,98 @@
 
 // Задача 1 - Чётное или нечётное
 int is_even(int n) {
-    // TODO: реализовать
+    return (n % 2 == 0); 
 }
 
 // Задача 2 - Максимум из трёх чисел
 int max_of_three(int a, int b, int c) {
-    // TODO: реализовать
+    int max = a;
+    
+    if (b > max) 
+    {
+        max = b;
+    }
+    if (c > max) 
+    {
+        max = c;
+    }
+    return max;
 }
 
-// Задача 3 - Знак числа
+// // Задача 3 - Знак числа
 int sign_of_number(int n) {
-    // TODO: реализовать
+    if (n > 0) 
+    {
+        return 1;
+    } 
+    else if (n < 0) 
+    {
+        return -1;
+    } 
+    else 
+    {
+        return 0;
+    }
 }
 
-// Задача 4 - Абсолютное значение
+// // Задача 4 - Абсолютное значение
 int absolute_value(int n) {
-    // TODO: реализовать
+    if (n < 0) 
+    {
+        return -n;
+    } else 
+    {
+        return n;
+    }
 }
 
 // Задача 5 - Треугольник существует
 int triangle_exists(int a, int b, int c) {
-    // TODO: реализовать
+
+    if ((b + c > a) && (a + c > b) && (a + b > c))
+        return 1;
+    return 0;
 }
 
 // Задача 6 - Класс оценок
 const char* get_grade(int score) {
-    // TODO: реализовать
+    if (score >= 90 && score <= 100) 
+    {
+        return "отлично";
+    } 
+    else if (score >= 75 && score <=90) 
+    {
+        return "хорошо";
+    } 
+    else if (score >= 60  && score <=74) 
+    {
+        return "удовлетворительно";
+    } 
+    else if (score >= 0 && score <=59) 
+    {
+        return "неудовлетворительно";
+    } 
+    else 
+    {
+        return "некорректно";
+    } 
 }
 
 // Задача 7 - Евклидово расстояние
 double euclidean_distance(double x1, double y1, double x2, double y2) {
-    // TODO: реализовать
+    double dx = x2 - x1;
+    double dy = y2 - y1;
+    return sqrt(dx * dx + dy * dy);
 }
 
 // Задача 8 - Манхэттенское расстояние
 int manhattan_distance(int x1, int y1, int x2, int y2) {
-    // TODO: реализовать
+    return absolute_value(x2 - x1) + absolute_value(y2 - y1);
 }
 
 // Вспомогательная функция для проверки простоты (можно писать сразу в задаче 9, но это плохой тон)
 int is_prime(int n) {
-    // TODO: реализовать
+        
 }
 
 // Задача 9 - Среднее арифметическое простых чисел в интервале
@@ -59,17 +110,71 @@ int count_multiples(int start, int end, int k) {
 
 // Задача 11 - Високосный год
 int is_leap_year(int year) {
-    // TODO: реализовать
+
+     if (year % 400 == 0) 
+    {
+        return 1;
+    } else if (year % 100 == 0)
+    {
+        return 0;
+    } 
+    else if (year % 4 == 0)
+    {
+        return 1;
+    } else 
+    {
+        return 0;
+    }
 }
 
 // Задача 12 - Сезон по номеру месяца
 const char* season_by_month(int month) {
-    // TODO: реализовать
+     switch (month) 
+    {
+        case 1:
+        case 2:
+        case 12:
+            return "зима";
+        case 3:
+        case 4:
+        case 5:
+            return "весна";
+        case 6:
+        case 7:
+        case 8:
+            return "лето";
+        case 9:
+        case 10:
+        case 11:
+            return "осень";
+        default:
+            return "некорректно";
+    }
 }
 
 // Задача 13 - День недели
 const char* day_of_week(int day_num) {
-    // TODO: реализовать
+    {
+    switch(day_num) 
+    {
+    case 1:
+        return "понедельник";
+    case 2:
+        return "вторник";
+    case 3:
+        return "среда";
+    case 4:
+        return "четверг";
+    case 5:
+        return "пятница";
+    case 6:
+        return "суббота";
+    case 7:
+        return "воскресенье";
+    default:
+    return "некорректно";
+    }
+}
 }
 
 // Задача 14 - Подсчёт цифр в числе
@@ -81,4 +186,3 @@ int count_digits(int n) {
 int reverse_number(int n) {
     // TODO: реализовать
 }
-
